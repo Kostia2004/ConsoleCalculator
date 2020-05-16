14c8285d1388aac37362cb9bfc7422cfab73ca7d
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-05-16 03:08:07 +0300

    input numbers

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,6 +6,15 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            string first;
+            string second;
+            int first_number;
+            int second_number;
+
+            Console.WriteLine("Enter first number: ");
+            first = Console.ReadLine();
+            Console.WriteLine("Enter second number: ");
+            second = Console.ReadLine();
         }
     }
 }
