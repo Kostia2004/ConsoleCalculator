97a0761bb3293da5042b0ee228aef7a109b10904
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-05-16 20:39:53 +0300

    added input operator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -13,6 +13,8 @@ namespace Calculator
 
             Console.WriteLine("Enter first number: ");
             first = Console.ReadLine();
+            Console.WriteLine("Enter operator: ");
+            string operate = Console.ReadLine();
             Console.WriteLine("Enter second number: ");
             second = Console.ReadLine();
             first_number = Convert.ToInt16(first);
